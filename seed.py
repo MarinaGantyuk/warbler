141facea59f5cc78c096735bb33f03ecedf3e948
MarinaGantyuk <Melany1689@yahoo.com>
2023-10-09 13:33:04 -0500

    first commit

---
+++ seed.py
@@ -0,0 +1,24 @@
+"""Seed database with sample data from CSV Files."""
+
+from csv import DictReader
+from app import db, app
+#from models import User, Message, Follows
+from models import db, connect_db, User, Message, Follows
+
+connect_db(app)
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    
+
+with open('generator/users.csv') as users:
+    db.session.bulk_insert_mappings(User, DictReader(users))
+
+with open('generator/messages.csv') as messages:
+    db.session.bulk_insert_mappings(Message, DictReader(messages))
+
+with open('generator/follows.csv') as follows:
+    db.session.bulk_insert_mappings(Follows, DictReader(follows))
+
+db.session.commit()
